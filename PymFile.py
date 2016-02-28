1b1e76d4b152a52ca138be1f8e31d26fec6954d8
Ryan Kim <Ryanfsdf@v1022-rn-170-200.campus-dynamic.uwaterloo.ca>
2016-02-28 15:33:37 -0500

    Added Regular Expression
    Works with all MP3/Flac/M4a files that have titles in their metadata.
    Uses the format AlbumName_TrackName_TrackNumber_ArtistName.

--- PymFile.py
+++ PymFile.py
@@ -1,27 +1,70 @@
 #! python3
 
 import os
+import re
 
 
 def check_file(file):
-    if file.endswith(".mp3"):
-        from mutagen.mp3 import EasyMP3
-        audio = EasyMP3(file)
-        new_name = audio["title"]
-        os.rename(file, str(new_name) + ".mp3")
-    if file.endswith(".flac"):
-        from mutagen.flac import FLAC
+    if file.lower().endswith(".mp3"):
         try:
+            from mutagen.mp3 import EasyMP3
+            audio = EasyMP3(file)
+            new_name = audio["title"]
+            new_artist = audio["artist"]
+            new_track_number = audio["tracknumber"]
+            new_album = audio["album"]
+            os.rename(file, re.sub('[^A-Za-z0-9]+', '', str(new_album)) + "_" +
+                      re.sub('[^A-Za-z0-9]+', '', str(new_name)) + "_" +
+                      re.sub('[^A-Za-z0-9]+', '', str(new_track_number)) + "_" +
+                      re.sub('[^A-Za-z0-9]+', '', str(new_artist)) + ".mp3")
+
+        except:
+            try:
+                from mutagen.mp3 import EasyMP3
+                audio = EasyMP3(file)
+                new_name = audio["title"]
+                new_artist = audio["artist"]
+                os.rename(file, re.sub('[^A-Za-z0-9]+', '', str(new_album)) + "_" +
+                          re.sub('[^A-Za-z0-9]+', '', str(new_name)) + "_" +
+                          re.sub('[^A-Za-z0-9]+', '', str(new_artist)) + ".mp3")
+            except:
+                try:
+                    from mutagen.mp3 import EasyMP3
+                    audio = EasyMP3(file)
+                    new_name = audio["title"]
+                    new_artist = audio["artist"]
+                    os.rename(file, re.sub('[^A-Za-z0-9]+', '', str(new_name)) + "_" +
+                              re.sub('[^A-Za-z0-9]+', '', str(new_artist)) + ".mp3")
+                except:
+                    print("Bad Mp3", file)
+    if file.lower().endswith(".flac"):
+        try:
+            from mutagen.flac import FLAC
             audio = FLAC(file)
             new_name = audio["title"]
-            os.rename(os.path.join('.', file), os.path.join('.', str(new_name) + '.flac'))
-        except KeyError:
+            new_artist = audio["artist"]
+            new_track_number = audio["tracknumber"]
+            new_album = audio["album"]
+            os.rename(file, re.sub('[^A-Za-z0-9]+', '', str(new_album)) + "_" +
+                      re.sub('[^A-Za-z0-9]+', '', str(new_name)) + "_" +
+                      re.sub('[^A-Za-z0-9]+', '', str(new_track_number)) + "_" +
+                      re.sub('[^A-Za-z0-9]+', '', str(new_artist)) + ".flac")
+        except:
             print("Bad Flac", file)
-    if file.endswith(".m4a"):
-        from mutagen.mp4 import MP4
-        audio = MP4(file)
-        new_name = audio['\xa9nam']
-        os.rename(os.path.join('.', file), os.path.join('.', str(new_name) + '.m4a'))
+    if file.lower().endswith(".m4a"):
+        try:
+            from mutagen.mp4 import MP4
+            audio = MP4(file)
+            new_name = audio['\xa9nam']
+            new_artist = audio['\xa9ART']
+            new_track_number = audio["trkn"]
+            new_album = audio['\xa9alb']
+            os.rename(file, re.sub('[^A-Za-z0-9]+', '', str(new_album)) + "_" +
+                      re.sub('[^A-Za-z0-9]+', '', str(new_name)) + "_" +
+                      re.sub('[^A-Za-z0-9]+', '', str(new_track_number)) + "_" +
+                      re.sub('[^A-Za-z0-9]+', '', str(new_artist)) + ".m4a")
+        except:
+            print("Bad m4a", file)
 
 for dirname, dirnames, filenames in os.walk('.'):
     for subdirname in dirnames:
