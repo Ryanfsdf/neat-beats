ae62950f25138efa6ac9f35af3e2de740b7cd1c2
Ryan Kim <Ryanfsdf@v1022-rn-170-200.campus-dynamic.uwaterloo.ca>
2016-03-08 02:45:48 -0500

    Added Organizer
    2 options now. To collapse the music folder and rename the music files.
    Or to organize the music files according to album/artist name

--- PymFile.py
+++ PymFile.py
@@ -2,6 +2,9 @@
 
 import os
 import re
+#TO DO - remove digit in track number for numbers larger than 2 digits
+# - make a safety catch to leave song alone if the title is less than 4 characters
+# - fix bad files not being put into 000 NO ALBUM SONGS
 
 
 def check_file(file):
@@ -10,66 +13,98 @@ def check_file(file):
             from mutagen.mp3 import EasyMP3
             audio = EasyMP3(file)
             new_name = audio["title"]
-            new_artist = audio["artist"]
             new_track_number = audio["tracknumber"]
-            new_album = audio["album"]
-            os.rename(file, re.sub('[^A-Za-z0-9]+', '', str(new_album)) + "_" +
-                      re.sub('[^A-Za-z0-9]+', '', str(new_name)) + "_" +
-                      re.sub('[^A-Za-z0-9]+', '', str(new_track_number)) + "_" +
-                      re.sub('[^A-Za-z0-9]+', '', str(new_artist)) + ".mp3")
-
+            os.rename(file, re.sub('[^A-Za-z0-9\s]+', '', str(new_track_number)) + " " +
+                      re.sub('[^A-Za-z0-9\s]+', '', str(new_name)) + ".mp3")
         except:
             try:
                 from mutagen.mp3 import EasyMP3
                 audio = EasyMP3(file)
                 new_name = audio["title"]
-                new_artist = audio["artist"]
-                os.rename(file, re.sub('[^A-Za-z0-9]+', '', str(new_album)) + "_" +
-                          re.sub('[^A-Za-z0-9]+', '', str(new_name)) + "_" +
-                          re.sub('[^A-Za-z0-9]+', '', str(new_artist)) + ".mp3")
+                os.rename(file, re.sub('[^A-Za-z0-9\s]+', '', str(new_name)) + ".mp3")
             except:
-                try:
-                    from mutagen.mp3 import EasyMP3
-                    audio = EasyMP3(file)
-                    new_name = audio["title"]
-                    new_artist = audio["artist"]
-                    os.rename(file, re.sub('[^A-Za-z0-9]+', '', str(new_name)) + "_" +
-                              re.sub('[^A-Za-z0-9]+', '', str(new_artist)) + ".mp3")
-                except:
-                    print("Bad Mp3", file)
+                print("Bad Mp3", file)
+
     if file.lower().endswith(".flac"):
         try:
             from mutagen.flac import FLAC
             audio = FLAC(file)
             new_name = audio["title"]
-            new_artist = audio["artist"]
             new_track_number = audio["tracknumber"]
-            new_album = audio["album"]
-            os.rename(file, re.sub('[^A-Za-z0-9]+', '', str(new_album)) + "_" +
-                      re.sub('[^A-Za-z0-9]+', '', str(new_name)) + "_" +
-                      re.sub('[^A-Za-z0-9]+', '', str(new_track_number)) + "_" +
-                      re.sub('[^A-Za-z0-9]+', '', str(new_artist)) + ".flac")
+            os.rename(file, re.sub('[^A-Za-z0-9\s]+', '', str(new_track_number)) + " " +
+                      re.sub('[^A-Za-z0-9\s]+', '', str(new_name)) + ".flac")
         except:
             print("Bad Flac", file)
+
     if file.lower().endswith(".m4a"):
         try:
             from mutagen.mp4 import MP4
             audio = MP4(file)
             new_name = audio['\xa9nam']
-            new_artist = audio['\xa9ART']
             new_track_number = audio["trkn"]
-            new_album = audio['\xa9alb']
-            os.rename(file, re.sub('[^A-Za-z0-9]+', '', str(new_album)) + "_" +
-                      re.sub('[^A-Za-z0-9]+', '', str(new_name)) + "_" +
-                      re.sub('[^A-Za-z0-9]+', '', str(new_track_number)) + "_" +
-                      re.sub('[^A-Za-z0-9]+', '', str(new_artist)) + ".m4a")
+            os.rename(file, re.sub('[^A-Za-z0-9\s]+', '', str(new_track_number)) + " " +
+                      re.sub('[^A-Za-z0-9\s]+', '', str(new_name)) + ".m4a")
         except:
             print("Bad m4a", file)
 
-response = input("run - change music file names" + os.linesep +
-                 "organize - make folders from your music files" + os.linesep +
-                 "quit - exit the program" + os.linesep +
-                 "Type your command:")
+
+def organize_file(file):
+    if file.lower().endswith(".mp3"):
+        try:
+            from mutagen.mp3 import EasyMP3
+            audio = EasyMP3(file)
+            new_artist = audio["artist"]
+            new_album = audio["album"]
+            try:
+                os.makedirs(re.sub('[^A-Za-z0-9\s]+', '', str(new_artist)) + " - " +
+                            re.sub('[^A-Za-z0-9\s]+', '', str(new_album)))
+            except:
+                try:
+                    os.makedirs("000 NO ALBUM SONGS")
+                    os.rename(file, os.path.join("000 NO ALBUM SONGS", file))
+                except: os.rename(file, os.path.join("000 NO ALBUM SONGS", file))
+            os.rename(file, os.path.join((re.sub('[^A-Za-z0-9\s]+', '', str(new_artist)) + " - " +
+                                          re.sub('[^A-Za-z0-9\s]+', '', str(new_album))), file))
+        except:
+            pass
+
+    if file.lower().endswith(".flac"):
+        try:
+            from mutagen.flac import FLAC
+            audio = FLAC(file)
+            new_artist = audio["artist"]
+            new_album = audio["album"]
+            try:
+                os.makedirs(re.sub('[^A-Za-z0-9\s]+', '', str(new_artist)) + " - " +
+                            re.sub('[^A-Za-z0-9\s]+', '', str(new_album)))
+            except:
+                try:
+                    os.makedirs("000 NO ALBUM SONGS")
+                    os.rename(file, os.path.join("000 NO ALBUM SONGS", file))
+                except: os.rename(file, os.path.join("000 NO ALBUM SONGS", file))
+            os.rename(file, os.path.join((re.sub('[^A-Za-z0-9\s]+', '', str(new_artist)) + " - " +
+                                          re.sub('[^A-Za-z0-9\s]+', '', str(new_album))), file))
+        except:
+            pass
+
+    if file.lower().endswith(".m4a"):
+        try:
+            from mutagen.mp4 import MP4
+            audio = MP4(file)
+            new_artist = audio['\xa9ART']
+            new_album = audio['\xa9alb']
+            try:
+                os.makedirs(re.sub('[^A-Za-z0-9\s]+', '', str(new_artist)) + " - " +
+                            re.sub('[^A-Za-z0-9\s]+', '', str(new_album)))
+            except:
+                try:
+                    os.makedirs("000 NO ALBUM SONGS")
+                    os.rename(file, os.path.join("000 NO ALBUM SONGS", file))
+                except: os.rename(file, os.path.join("000 NO ALBUM SONGS", file))
+            os.rename(file, os.path.join((re.sub('[^A-Za-z0-9\s]+', '', str(new_artist)) + " - " +
+                                          re.sub('[^A-Za-z0-9\s]+', '', str(new_album))), file))
+        except:
+            pass
 
 
 def type_input(runfunc):
@@ -80,16 +115,43 @@ def type_input(runfunc):
                     check_file(os.path.join(dirname, subdirname))
                 for filename in filenames:
                     check_file(os.path.join(dirname, filename))
-        else:
-            type_input(input("run - change music file names" + os.linesep +
-                             "organize - make folders from your music files" + os.linesep +
+            print("Successful")
+            type_input(input("organize - Organize all music files into folders for "
+                             "their corresponding album/artist name" + os.linesep +
                              "quit - exit the program" + os.linesep +
                              "Type your command:"))
-
+        else:
+            type_input(input("run - Collapse all folders and rename music files" + os.linesep +
+                             "organize - Organize all music files into folders for "
+                             "their corresponding album/artist name" + os.linesep +
+                             "quit - exit the program" + os.linesep +
+                             "Type your command:"))
+    if runfunc == "organize":
+        if "yes" == input("Are you sure? Make sure you back up your files first (yes/no):"):
+            for dirname, dirnames, filenames in os.walk('.'):
+                for subdirname in dirnames:
+                    organize_file(os.path.join(dirname, subdirname))
+                for filename in filenames:
+                    organize_file(os.path.join(dirname, filename))
+            print("Successful")
+            type_input(input("run - Collapse all folders and rename music files" + os.linesep +
+                             "quit - exit the program" + os.linesep +
+                             "Type your command:"))
+        else:
+            type_input(input("run - Collapse all folders and rename music files" + os.linesep +
+                             "organize - Organize all music files into folders for "
+                             "their corresponding album/artist name" + os.linesep +
+                             "quit - exit the program" + os.linesep +
+                             "Type your command:"))
     elif runfunc == "quit":
         pass
 
     else:
         type_input(input("Invalid Command" + ''' " ''' + runfunc + ''' " :'''))
 
+response = input("run - Collapse all folders and rename music files" + os.linesep +
+                 "organize - Organize all music files into folders for "
+                 "their corresponding album/artist name" + os.linesep +
+                 "quit - exit the program" + os.linesep +
+                 "Type your command:")
 type_input(response)
