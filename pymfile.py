22a4fbbccc70d0a6df178e831c7856b824889ccf
Ryan Kim <Ryanfsdf@v1022-rn-170-200.campus-dynamic.uwaterloo.ca>
2016-03-13 20:09:21 -0400

    Added back pymfile

---
+++ pymfile.py
@@ -0,0 +1,16 @@
+#! python3
+
+import os
+import get_input
+
+
+# TO DO - remove digit in track number for numbers larger than 2 digits
+# - make a safety catch to leave song alone if the title is less than 4 characters
+# - fix bad files not being put into 000 NO ALBUM SONGS
+
+response = input("run - Collapse all folders and rename music files" + os.linesep +
+                 "organize - Organize all music files into folders for "
+                 "their corresponding album/artist name" + os.linesep +
+                 "quit - exit the program" + os.linesep +
+                 "Type your command:")
+get_input.type_input(response)
